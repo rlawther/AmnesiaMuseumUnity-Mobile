9d55f8729e42766dd6aeee6fbb9429ddd0d9bff7
rlawther <rlawther@cse.unsw.edu.au>
2014-08-27 12:47:07 +1000

    billboard radius and animation curves per episode

--- Assets/BillboardAndFade.cs
+++ Assets/BillboardAndFade.cs
@@ -4,9 +4,16 @@ using System.Collections;
 public class BillboardAndFade : MonoBehaviour {
 
 	public bool applyBillboard;
-	public float billboardRadius;
+	public float []billboardRadius;
 	public bool applyFade;
-	public AnimationCurve fadeCurve;
+	public AnimationCurve []fadeCurve;
+	public AnimationCurve fadeCurveG1E1;
+	public AnimationCurve fadeCurveG1E2;
+	public AnimationCurve fadeCurveG1E3;
+	public AnimationCurve fadeCurveG2E1;
+	public AnimationCurve fadeCurveG2E2;
+	public AnimationCurve fadeCurveG2E3;
+	public AnimationCurve fadeCurveG2E4;
 	public float fadeDistanceMultiplier;
 
 	private VisualizerManager visManager;
@@ -18,11 +25,19 @@ public class BillboardAndFade : MonoBehaviour {
 	void Start () {
 		this.visManager = gameObject.GetComponent<VisualizerManager> ();
 		player = GameObject.Find ("First Person Controller");
-		
+		fadeCurve = new AnimationCurve[7];
+		fadeCurve[0] = fadeCurveG1E1;
+		fadeCurve[1] = fadeCurveG1E2;
+		fadeCurve[2] = fadeCurveG1E3;
+		fadeCurve[3] = fadeCurveG2E1;
+		fadeCurve[4] = fadeCurveG2E2;
+		fadeCurve[5] = fadeCurveG2E3;
+		fadeCurve[6] = fadeCurveG2E4;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		int episodeNum = 0;
 		if (player == null)
 		{
 			player = GameObject.Find ("First Person Controller");
@@ -44,20 +59,21 @@ public class BillboardAndFade : MonoBehaviour {
 					if (applyFade)
 					{
 						photo.gameObject.renderer.material.color = 
-							new Color (1.0f, 1.0f, 1.0f, fadeCurve.Evaluate(distance / fadeDistanceMultiplier));
+							new Color (1.0f, 1.0f, 1.0f, fadeCurve[episodeNum].Evaluate(distance / fadeDistanceMultiplier));
 					}
 					if (applyBillboard)
 					{
-						if (distance < billboardRadius)
+						if (distance < billboardRadius[episodeNum])
 						{
 							photo.LookAt(new Vector3(player.transform.position.x,
 							                         photo.position.y,
 							                         player.transform.position.z));
 						}
 					}
+					
 				}
+				episodeNum++;
 			}
-			
 		}
 		
 	}
